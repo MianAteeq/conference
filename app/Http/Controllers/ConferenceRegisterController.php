540558a132bfdbc71afa70ff29ab1527eb83fa9c
ateeqadrees@ <ateeqadrees@fissionmonster.com>
2025-12-23 12:21:36 +0500

    bug fix

--- app/Http/Controllers/ConferenceRegisterController.php
+++ app/Http/Controllers/ConferenceRegisterController.php
@@ -29,7 +29,7 @@ class ConferenceRegisterController extends Controller
         $request->validate([
             'first_name' => 'required|string|max:255',
             'last_name'  => 'required|string|max:255',
-            'email'      => 'required|email|max:255',
+            // 'email'      => 'required|email|max:255',
             'phone'      => 'required|string|max:20',
             'message'    => 'required|string',
         ]);
