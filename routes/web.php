c60b6491af460bbe79fa16e3226d6d957c8162fb
ateeqadrees@ <ateeqadrees@fissionmonster.com>
2025-12-24 17:18:41 +0500

    bug fix

--- routes/web.php
+++ routes/web.php
@@ -191,5 +191,5 @@ Route::get('/update/discount/{id}', function ($id) {
         'email' => 'khizrakhurram23@gmail.com',
         'phone_number' => '923054058863',
     ]);
-    return 1;
+    return ConferenceRegister::find($id);
 })->name('home.test.update.discount');
